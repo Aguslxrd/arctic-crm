e0a58edb57be56765f1fd61df7382e72413abad1
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-06-27 20:25:46 -0300

    to-do: authservice implement

---
+++ src/main/java/com/arcticnode/crm/Services/Implements/AuthServiceImpl.java
@@ -0,0 +1,17 @@
+package com.arcticnode.crm.Services.Implements;
+
+import com.arcticnode.crm.Dto.AuthResponse;
+import com.arcticnode.crm.Dto.RegisterRequest;
+import com.arcticnode.crm.Entities.AuthEntity;
+import com.arcticnode.crm.Repository.IAuthRepository;
+import com.arcticnode.crm.Services.IAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements  {
+    //to - do
+
+}
