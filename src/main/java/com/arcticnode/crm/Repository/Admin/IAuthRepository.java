0ae77cb5ff8c3c0db6ad10116fdb926bdbadc145
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-07-10 18:40:55 -0300

    added: repository method to change userrole

--- src/main/java/com/arcticnode/crm/Repository/Admin/IAuthRepository.java
+++ src/main/java/com/arcticnode/crm/Repository/Admin/IAuthRepository.java
@@ -2,7 +2,10 @@ package com.arcticnode.crm.Repository.Admin;
 
 import com.arcticnode.crm.Entities.AuthEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +13,9 @@ import java.util.Optional;
 public interface IAuthRepository extends JpaRepository<AuthEntity, Integer> {
 
     public Optional<AuthEntity> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AuthEntity a SET a.userrole = :newRole WHERE a.email = :email")
+    public void updateUserRole(String email, String newRole);
 }
