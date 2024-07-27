f3945e9b9ac93522e8583862b3d20d5fd6df4a0e
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-07-27 14:24:33 -0300

    added: logging entity

--- src/main/java/com/arcticnode/crm/Entities/LoggingEntity.java
+++ src/main/java/com/arcticnode/crm/Entities/LoggingEntity.java
@@ -1,10 +1,12 @@
 package com.arcticnode.crm.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "logging")
@@ -12,4 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoggingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer log_id;
+    @Column(name = "user_id")
+    private Integer userId;
+    private String user_action;
+    private String details;
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
