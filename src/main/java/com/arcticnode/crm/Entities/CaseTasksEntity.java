929754d5760a26a443d701ab9e34ac4fbba21285
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-09-21 10:54:41 -0300

    tabla relacional de tareas en tabla casos - spring

---
+++ src/main/java/com/arcticnode/crm/Entities/CaseTasksEntity.java
@@ -0,0 +1,34 @@
+package com.arcticnode.crm.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "case_tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaseTasksEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer task_id;
+
+    @Column(nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private Integer authId;
+
+    @Column(nullable = false, length = 120)
+    private String task_content;
+
+    private LocalDateTime task_date;
+}
